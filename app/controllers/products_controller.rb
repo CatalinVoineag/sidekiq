01f0e0ef086b2d4d4cc7101b994fe3ee2714e14b
dwarfi <catalin94v@gmail.com>
2017-08-30 01:04:22 +0100

    working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,13 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def export_csv
+  	 
+     options = { depot_id: Depot.current, record_id: @export_form.id, record_name: @export_form.class.name, model: @export_form.model, company_id: @export_form.company_id, queue: 'export_csv', priority: 0, obj_ids: obj_ids, required_pairs: required_pairs, user_id: User.current, title: title} 
+     ExportFormWorker.perform_async(options)
+     
+     flash[:notice] = "Processing Job. Please wait"
+     redirect_to background_jobs_path
+  end
+
 end
